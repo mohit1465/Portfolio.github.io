640ac657c5268c11cff011c503ecdfd759190e6a
Mohit Yadav <mohit8307521465@gmail.com>
2024-07-12 00:13:09 +0530

    y

--- script.js
+++ script.js
@@ -1,10 +1,57 @@
-// Mobile Menu Toggle
+// Function to close mobile menu
 document.getElementById('hamburger').addEventListener('click', function() {
+    this.classList.toggle('cross');
+    var mobNavs = document.querySelectorAll('.mob-nav');
+    for (var i = 0; i < mobNavs.length; i++) {
+        if (this.classList.contains('cross')) {
+            mobNavs[i].style.display = 'flex';
+            mobNavs[i].style.animation = 'slideInRight 0.5s forwards';
+        } else {
+            mobNavs[i].style.animation = 'none';
+            mobNavs[i].style.display = 'none';
+        }
+    }
+});
+
+
+
+function closeMobileMenu() {
     const mobileMenu = document.getElementById('mobile-menu');
-    if (mobileMenu.style.display === 'flex') {
-        mobileMenu.style.display = 'none';
-    } else {
+    mobileMenu.style.display = 'none';
+}
+
+// Mobile Menu Toggle
+document.getElementById('hamburger').addEventListener('click', function(event) {
+    event.stopPropagation(); // Prevents the click from bubbling up and triggering the document click listener
+    const mobileMenu = document.getElementById('mobile-menu');
+    if (window.getComputedStyle(mobileMenu).display === 'none') {
         mobileMenu.style.display = 'flex';
+    } else {
+        closeMobileMenu();
+    }
+});
+
+// Close mobile menu when clicking navigation items
+const navLinks = document.querySelectorAll('#mobile-menu ul li a');
+navLinks.forEach(function(navLink) {
+    navLink.addEventListener('click', function() {
+        closeMobileMenu();
+    });
+});
+
+// Close mobile menu when switching from mobile to desktop view
+window.addEventListener('resize', function() {
+    if (window.innerWidth >= 768) { // Assuming 768px is the breakpoint for desktop view
+        closeMobileMenu();
+    }
+});
+
+// Close mobile menu when clicking outside of it
+document.addEventListener('click', function(event) {
+    const mobileMenu = document.getElementById('mobile-menu');
+    const hamburger = document.getElementById('hamburger');
+    if (!mobileMenu.contains(event.target) && event.target !== hamburger) {
+        mobileMenu.style.display = 'none';
     }
 });
 
