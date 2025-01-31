e2d953618267d92fb5254cc8c13eeca4526a157d
Mohit Yadav <90053172+mohit1465@users.noreply.github.com>
2025-01-31 15:11:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 var animationSpeed = 1000; // Adjust animation speed in milliseconds (1s = 1000ms)
 
-// document.addEventListener('contextmenu', event=> event.preventDefault()); 
-// document.onkeydown = function(e) { 
-// if(event.keyCode == 123) { 
-// return false; 
-// } 
-// if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){ 
-// return false; 
-// } 
-// if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){ 
-// return false; 
-// } 
-// if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){ 
-// return false; 
-// } 
-// } 
+document.addEventListener('contextmenu', event=> event.preventDefault()); 
+document.onkeydown = function(e) { 
+if(event.keyCode == 123) { 
+return false; 
+} 
+if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){ 
+return false; 
+} 
+if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){ 
+return false; 
+} 
+if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){ 
+return false; 
+} 
+} 
 
 function toggleMenu() {
     var hamburger = document.getElementById('hamburger');
